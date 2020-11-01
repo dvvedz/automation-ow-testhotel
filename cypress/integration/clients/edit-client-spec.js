d06fa68324482c9018942d85bdf6aa18c65e1063
dvvedz <oskar9899@gmail.com>
2020-11-01 20:09:00 +0100

    add correct reports

--- cypress/integration/clients/edit-client-spec.js
+++ cypress/integration/clients/edit-client-spec.js
@@ -5,6 +5,7 @@ import { CreateClient, EditClient } from "../models/clientes-page-util"
 describe("Edit client", () => {
     const ec = new EditClient("TestEdit", "TestEdit@mail.com", "0123456789")
     const cc = new CreateClient("TestEdit", "TestEdit@mail.com", "0123456789")
+
     beforeEach(() => {
         cy.login()
     })
