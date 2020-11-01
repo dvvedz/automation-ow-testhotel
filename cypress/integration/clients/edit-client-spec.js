a82c99917b782ad9325fed9d55bdc2abda8c51e2
dvvedz <oskar9899@gmail.com>
2020-11-01 21:33:30 +0100

    last commit

--- cypress/integration/clients/edit-client-spec.js
+++ cypress/integration/clients/edit-client-spec.js
@@ -1,17 +1,17 @@
 
 import { visitEachChild } from "typescript";
-import { CreateClient, EditClient } from "../models/clientes-page-util"
+import { CreateClient, EditClient } from "../models/clientes-page-util";
 
 describe("Edit client", () => {
-    const ec = new EditClient("TestEdit", "TestEdit@mail.com", "0123456789")
-    const cc = new CreateClient("TestEdit", "TestEdit@mail.com", "0123456789")
+    const ec = new EditClient("TestEdit", "TestEdit@mail.com", "0123456789");
+    const cc = new CreateClient("TestEdit", "TestEdit@mail.com", "0123456789");
 
     beforeEach(() => {
-        cy.login()
+        cy.login();
     })
     it("Edits a client", () => {
         cy.createClientApi("createdFrom Api", "test@mail.com", "012345678");
-        ec.edit()
-        cc.isCreated()
+        ec.edit();
+        cc.isCreated();
     });
 });
